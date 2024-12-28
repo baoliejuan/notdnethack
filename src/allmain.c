8ab53bd2a64bbaf69a624748a7cb64a617b3df66
Ron Nazarov <ron@noisytoot.org>
2024-12-28 19:48:56 +0000

    Make LIMIT_IPS a compile-time option
    Disabled by default.  This should be enabled in local.mk in the
    hardfought build.  Limiting command repeats and instructions per
    second is annoying and unnecessary when playing locally or with proper
    CPU usage limiting (cgroups).

--- src/allmain.c
+++ src/allmain.c
@@ -1823,7 +1823,9 @@ moveloop()
 	// printSanAndInsight();
 	// printEtraits();
     for(;;) {/////////////////////////MAIN LOOP/////////////////////////////////
+#ifdef LIMIT_IPS
 	if (!iflags.debug_fuzzer) gosleep();
+#endif
     hpDiff = u.uhp;
 	get_nh_event();
 #ifdef POSITIONBAR
